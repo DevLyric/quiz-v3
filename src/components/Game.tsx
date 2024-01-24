4f3c919d3e45bb7c1194578316c5c40630ab3665
Douglas Souza <lyricsan@hotmail.com>
2024-01-24 15:50:29 -0300

    working on game

---
+++ src/components/Game.tsx
@@ -0,0 +1,10 @@
+export default function Game() {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-semibold">Question</h1>
+      <div className="flex flex-col gap-3 text-center mt-8">
+        <button>Answer</button>
+      </div>
+    </>
+  );
+}
