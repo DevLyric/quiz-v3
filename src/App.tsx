db37e7941c3e5eff6238d54c09533038fad9cfa2
Douglas Souza <lyricsan@hotmail.com>
2024-01-24 11:04:04 -0300

    working on question type selection page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import QuestionTypeSelection from "./pages/QuestionTypeSelection";
 
 export default function App() {
   return (
@@ -9,6 +10,10 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route
+            path="/question-type-selection"
+            element={<QuestionTypeSelection />}
+          />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/QuestionTypeSelection.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+export default function QuestionTypeSelection() {
+  return (
+    <div className="mt-32">
+      <div className="container mx-auto max-w-xl px-6">
+        <h1 className="text-2xl text-center font-semibold mb-4">
+          Choose Question Type
+        </h1>
+        <p className="text-center text-gray-600">
+          Select the type of questions you want to receive and let the quiz
+          begin!
+        </p>
+
+        <div className="flex flex-col justify-center text-center md:flex-row gap-4 mt-8">
+          <Link
+            to="/questions/general"
+            className="rounded-full py-3 px-8 bg-blue-500 text-white hover:scale-105 transition-all"
+          >
+            General Knowledge
+          </Link>
+          <Link
+            to="/questions/science"
+            className="rounded-full py-3 px-8 bg-green-500 text-white hover:scale-105 transition-all"
+          >
+            Science
+          </Link>
+          <Link
+            to="/questions/movies"
+            className="rounded-full py-3 px-8 bg-yellow-500 text-white hover:scale-105 transition-all"
+          >
+            Movies
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
