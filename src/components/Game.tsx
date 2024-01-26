9572bbd1618d19e7fdd11692ac88b2b36fc59e37
Douglas Souza <lyricsan@hotmail.com>
2024-01-26 13:24:06 -0300

    game added

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,23 @@
-export default function Game() {
+interface IGame {
+  question: string;
+  options: string[];
+  onSelectAnswer: (option: string) => void;
+}
+
+export default function Game({ question, options, onSelectAnswer }: IGame) {
   return (
     <>
-      <h1 className="text-3xl text-center font-semibold">Question</h1>
+      <h1 className="text-3xl text-center font-semibold">{question}</h1>
       <div className="flex flex-col gap-3 text-center mt-8">
-        <button>Answer</button>
+        {options.map((option) => (
+          <button
+            onClick={() => onSelectAnswer(option)}
+            key={option}
+            className="rounded p-2 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 text-white hover:scale-105 transition-all"
+          >
+            {option}
+          </button>
+        ))}
       </div>
     </>
   );
