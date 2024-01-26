2dbe35013db71c910540e830dd960b2db242fdac
Douglas Souza <lyricsan@hotmail.com>
2024-01-26 13:24:22 -0300

    game over added

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -1,3 +1,29 @@
-export default function GameOver() {
-  return <div>Congratulations, you got 8/10 right</div>;
+import { Link } from "react-router-dom";
+
+interface IGameOver {
+  score: number;
+  numberOfQuestions: number;
+}
+
+export default function GameOver({ score, numberOfQuestions }: IGameOver) {
+  return (
+    <>
+      <h1 className="text-center text-2xl font-semibold mb-4">
+        Congratulations! You answered {score} out of {numberOfQuestions}{" "}
+        questions correctly.
+      </h1>
+
+      <p className="text-center">
+        If you'd like to see your name on the leaderboard,{" "}
+        <Link to="/auth/login" className="text-blue-500 font-semibold">
+          log in
+        </Link>{" "}
+        or{" "}
+        <Link to="/auth/register" className="text-blue-500 font-semibold">
+          sign up
+        </Link>{" "}
+        now!
+      </p>
+    </>
+  );
 }
