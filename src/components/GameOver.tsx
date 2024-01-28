f076fc0097391e3d4853fb186c107071906fb63d
Douglas Souza <lyricsan@hotmail.com>
2024-01-28 13:07:23 -0300

    changing path

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -15,11 +15,11 @@ export default function GameOver({ score, numberOfQuestions }: IGameOver) {
 
       <p className="text-center">
         If you'd like to see your name on the leaderboard,{" "}
-        <Link to="/auth/login" className="text-blue-500 font-semibold">
+        <Link to="/signIn" className="text-blue-500 font-semibold">
           log in
         </Link>{" "}
         or{" "}
-        <Link to="/auth/register" className="text-blue-500 font-semibold">
+        <Link to="/signUp" className="text-blue-500 font-semibold">
           sign up
         </Link>{" "}
         now!
