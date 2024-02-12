15d4b7616a7e1a47a3b8002e9a249ded6bdc9783
Douglas Souza <lyricsan@hotmail.com>
2024-02-12 02:07:30 -0300

    add GoogleIcon to AuthButton

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,3 +1,5 @@
+import { GoogleIcon } from './icons/GoogleIcon'
+
 interface AuthButtonProps {
   onClick: () => void
 }
@@ -8,6 +10,7 @@ export function AuthButton({ onClick }: AuthButtonProps) {
       onClick={onClick}
       className="flex items-center gap-3 rounded w-full py-3 px-6 border my-6 text-sm font-medium"
     >
+      <GoogleIcon />
       Continue with Google
     </button>
   )
